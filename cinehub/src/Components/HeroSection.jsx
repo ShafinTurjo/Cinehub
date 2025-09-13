952664bb1cd2fcc51fbdbcb6632da58e3005f3ff
ShafinTurjo <shafin.cse.20230104132@aust.edu>
2025-09-13 06:30:32 +0600

    3rd

--- cinehub/src/Components/HeroSection.jsx
+++ cinehub/src/Components/HeroSection.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { assets } from '../assets/assets';
 import { CalendarIcon, ClockIcon } from 'lucide-react';
-import bgImg from '../assets/backgroundImage.jpeg';
+import bgImg from '../assets/backgroundImage.png';
 import {useNavigate} from 'react-router-dom';
 import { ArrowRight } from 'lucide-react';
 

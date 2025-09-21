16921287da0c242431c6ffe5654838962734ea10
ShafinTurjo <shafin.cse.20230104132@aust.edu>
2025-09-21 23:33:31 +0600

    update

--- cinehub/src/context/AppContext.jsx
+++ cinehub/src/context/AppContext.jsx
@@ -1,8 +1,8 @@
 import { useContext, useState, createContext, useEffect } from "react";
 import axios from "axios";
-import { useAuth, useUser } from "@clerk/clerk-react";
 import { useLocation, useNavigate } from "react-router-dom";
 import toast from "react-hot-toast";
+
 
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
@@ -10,16 +10,29 @@ export const AppContext = createContext();
 
 export const AppProvider = ({ children }) => {
   const [isAdmin, setIsAdmin] = useState(false);
-  const { user } = useUser();
-  const { getToken } = useAuth();
+  const [user, setUser] = useState(null); 
+  const [token, setToken] = useState(null); 
   const location = useLocation();
   const navigate = useNavigate();
+
+  useEffect(() => {
+    const storedUser = JSON.parse(localStorage.getItem("user"));
+    const storedToken = localStorage.getItem("token");
+
+    if (storedUser && storedToken) {
+      setUser(storedUser);
+      setToken(storedToken);
+    }
+  }, []);
 
   const fetchIsAdmin = async () => {
     try {
       const { data } = await axios.get("/api/admin/is-admin", {
-        headers: { Authorization: `Bearer ${await getToken()}`},
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
       });
+
       setIsAdmin(data.isAdmin);
 
       if (!data.isAdmin && location.pathname.startsWith("/admin")) {
@@ -27,23 +40,25 @@ export const AppProvider = ({ children }) => {
         toast.error("You are not authorized to access admin dashboard");
       }
     } catch (error) {
-      console.error(error);
+      console.error("Error checking admin status:", error);
     }
   };
 
   useEffect(() => {
-    if (user) {
+    if (user && token) {
       fetchIsAdmin();
     }
-  }, [user]);
+  }, [user, token]);
 
   const value = {
     axios,
-    fetchIsAdmin,
     user,
-    getToken,
+    token,
+    setUser,
+    setToken,
     navigate,
     isAdmin,
+    fetchIsAdmin,
   };
 
   return (
